40617ba9065332762a3149165452b21b70d5b961
algirdaskuslys <algirdas.kuslys@kitm.lt>
2020-11-30 16:08:35 +0200

    added remember me functionality.

--- app/src/main/java/com/corona/coronazp20/LoginActivity.java
+++ app/src/main/java/com/corona/coronazp20/LoginActivity.java
@@ -5,8 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,31 +21,41 @@ public class LoginActivity extends AppCompatActivity {
 
         Button login = findViewById(R.id.login);
 
+        final CheckBox rememberme = (CheckBox) findViewById(R.id.rememberMe);
+        //bus konstruojamas vartotojo objektas perduodant context'a (langa kuriame esame)
+        final User user=new User(LoginActivity.this);
+        //patikriname, ar paskutini karta buvo pazymetas checkbox remember me
+        rememberme.setChecked(user.isRememberedForLogin());
+
+        //Aprasoma prisiminti mane checkbox logika
+        if (rememberme.isChecked()){//jeigu checkbox buvo pazymetas-vartotojas pageidavo, kad informacija buvo issaugota
+            username.setText(user.getUsernameForLogin(),TextView.BufferType.EDITABLE);//setText-uzpildysime user informacija, editable - suteiksim galimybe paredaguoti duomenis
+            password.setText(user.getPasswordForLogin(),TextView.BufferType.EDITABLE);
+        } else {//jeigu checkbox buvo nepazymetas-vartotojas nenorejo, kad informacija buvo issaugota
+            username.setText("", TextView.BufferType.EDITABLE);
+            password.setText("", TextView.BufferType.EDITABLE);
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // čia bus vykdomas kodas, kai paspaudžiamas mygtukas
-                // Kuriamas User klases objektas
-                //  public User(String username, String password)
-                User user=new User(username.getText().toString(),password.getText().toString());
-
-                Toast.makeText(LoginActivity.this,"prisijungimo vardas:"+
-                        user.getUsername() + "\n" + "slaptazodis:" +
-                        user.getPassword(), Toast.LENGTH_LONG).show();
-                Toast.makeText(LoginActivity.this,
-                        username.getText().toString(),
-                        Toast.LENGTH_SHORT).show();
 
                 username.setError(null);
-                if(Validation.isValidUsername(username.getText().toString())) {
+                if(Validation.isValidUsername(username.getText().toString()) && Validation.isValidPassword(password.getText().toString())) {
+                    user.setUsernameForLogin(username.getText().toString());
+                    user.setPasswordForLogin(password.getText().toString());
+                    if (rememberme.isChecked()){
+                        user.setRemembermeKeyForLogin(true);
+                    } else {
+                        user.setRemembermeKeyForLogin(false);
+                    }
                     Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class);
                     startActivity(goToSearchActivity);
-                } else { // jeigu username neteisingas
-                    username.setError("Error! Wrong username");
+                } else { // jeigu username arba password neteisingas
+                    username.setError("Error! Wrong username or/ and password");
                     username.requestFocus();
                 }
-
-
             }
         });
 
--- app/src/main/java/com/corona/coronazp20/Validation.java
+++ app/src/main/java/com/corona/coronazp20/Validation.java
@@ -5,10 +5,18 @@ import java.util.regex.Pattern;
 
 public class Validation {
     public static final String REGEX_USERNAME_PATTERN = "^[a-zA-Z]{3,20}$";
+    public static final String REGEX_PASSWORD_PATTERN = "^[a-zA-Z]{3,20}$";
 
     public static boolean isValidUsername(String username){
         Pattern pattern = Pattern.compile(REGEX_USERNAME_PATTERN);
         Matcher matcher = pattern.matcher(username);
+
+        return matcher.find();
+    }
+
+    public static boolean isValidPassword(String password){
+        Pattern pattern = Pattern.compile(REGEX_PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
 
         return matcher.find();
     }
